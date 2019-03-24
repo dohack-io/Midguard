f4ec9f44412a39fbea0023a18b6bd2a30a97b990
steffen <steffenosterburg@googlemail.com>
2019-03-24 04:08:49 +0100

    added messenger to auth-gateway an pm2 config

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -51,5 +51,18 @@ module.exports = {
     env_production: {
       NODE_ENV: 'production'
     },
+  },{
+    name: 'messenger',
+    script: './messenger/server.js',
+    instances: 1,
+    autorestart: false,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    },
   }]
 };
